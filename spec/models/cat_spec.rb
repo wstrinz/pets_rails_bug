d7e1b2d2c1df7e0f2452cfeed7bf6cc7ddf47fdf
Will Strinz <wstrinz@gmail.com>
2018-05-04 12:11:02 -0500

    specs for adjacent cases

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -13,7 +13,17 @@ RSpec.describe Cat, type: :model do
     expect(cat.dog).to eq(dog)
   end
 
-  it "can update dog name" do
-    cat.dog.update!(name: "fwuffy")
+  it "raises stack error when updating through instance reference" do
+    expect{cat.dog.update!(name: "fwuffy")}.to raise_error(SystemStackError)
+  end
+
+  it "raises stack error when calls are not chained" do
+    d = cat.dog
+    expect{d.update!(name: "fwuffy")}.to raise_error(SystemStackError)
+  end
+
+  it "does not raise stack error when reloading before updating" do
+    cat.dog.reload.update!(name: "fwuffy")
+    expect(cat.dog.name).to eq("fwuffy")
   end
 end
